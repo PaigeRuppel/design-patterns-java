ee658731f90dad97178cd49bc5a6c5c0fecff4d7
Paige Ruppel <paige.ruppel@gmail.com>
2018-02-14 15:17:38 -0500

    added main method to OCP web shopping app demo

--- out/production/resources/SOLID-Notes.md
+++ out/production/resources/SOLID-Notes.md
@@ -4,4 +4,7 @@
 * A class should only have one reason to change
 * If you have to use "and" or "or" when describing a class, that is a sign you might be violating SRP
 * Separate your concerns
+* Avoid creating "god objects" - they become difficult to maintain
 
+##Open Closed Principle (OCP)
+* Should be open for extension but closed for modification
--- src/main/java/WebShoppingApp.java
+++ src/main/java/WebShoppingApp.java
@@ -1,7 +1,25 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class WebShoppingApp {
+    public static void main(String[] args) {
+        Product apple = new Product("Apple", Color.GREEN, Size.SMALL);
+        Product tree = new Product("Tree", Color.GREEN, Size.LARGE);
+        Product firetruck = new Product("Firetruck", Color.RED, Size.LARGE);
+        Product house = new Product("House", Color.BLUE, Size.YUGE);
+
+        List<Product> products = new ArrayList<>();
+        products.add(apple);
+        products.add(tree);
+        products.add(firetruck);
+        products.add(house);
+
+        ProductFilter pf = new ProductFilter();
+
+        System.out.println("Green products (old): ");
+        pf.filterByColor(products, Color.GREEN).forEach(p -> System.out.println(" - " + p.getName() + " is green"));
+    }
 }
 
 enum Color {
