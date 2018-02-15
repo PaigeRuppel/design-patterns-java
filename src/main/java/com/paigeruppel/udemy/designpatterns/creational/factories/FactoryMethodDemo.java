17f73eee16895db9b7935adb902bf6cf82f74314
Paige Ruppel <paige.ruppel@gmail.com>
2018-02-15 14:25:59 -0500

    point between factory method and factory

--- src/main/java/com/paigeruppel/udemy/designpatterns/creational/factories/FactoryMethodDemo.java
+++ src/main/java/com/paigeruppel/udemy/designpatterns/creational/factories/FactoryMethodDemo.java
@@ -1,35 +1,42 @@
 package com.paigeruppel.udemy.designpatterns.creational.factories;
 
 public class FactoryMethodDemo {
+    Point p = Point.newPolarPoint(2, 3);
 }
 
-enum CoordinateSystem{
-    CARTESIAN, POLAR
-}
 class Point {
     private double x, y;
 
-// This is ugly
-    public Point(double a, double b, CoordinateSystem cs) {
-        switch (cs) {
-            case CARTESIAN:
-                x = a;
-                y = b;
-                break;
-            case POLAR:
-                x = a * Math.cos(b);
-                y = a * Math.sin(b);
-        }
-    }
-
- /*   public Point(double x, double y) {
+    // hide your constructor so that the user has to explicitly call one of the factory methods
+    private Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-  Would be nice to do this... but you can't
-    public Point(double rho, double theta) {
-        x = rho * Math.cos(theta);
-        y = rho * Math.sin(theta);
-    }*/
+    // factory methods allow you to clearly name your signatures and parameters
+
+
+    /*   public Point(double x, double y) {
+           this.x = x;
+           this.y = y;
+       }
+
+     Would be nice to do this... but you can't
+       public Point(double rho, double theta) {
+           x = rho * Math.cos(theta);
+           y = rho * Math.sin(theta);
+       }*/
+}
+
+// another option is to have all of your factory methods grouped under one factory class
+class PointFactory {
+    public static Point newCartesianPoint(double x, double y) {
+        return new Point(x, y);
+    }
+
+    public static Point newPolarPoint(double rho, double theta) {
+        double x = rho * Math.cos(theta);
+        double y = rho * Math.sin(theta);
+        return new Point(x, y);
+    }
 }
