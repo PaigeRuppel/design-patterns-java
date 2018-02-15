47453148e88fab3df06ef67c4091b22dbcee440c
Paige Ruppel <paige.ruppel@gmail.com>
2018-02-15 12:35:47 -0500

    started coding solution for builder code exercise

---
+++ src/main/java/com/paigeruppel/udemy/designpatterns/creational/builder/BuilderCodeExercise.java
@@ -0,0 +1,35 @@
+package com.paigeruppel.udemy.designpatterns.creational.builder;
+
+public class BuilderCodeExercise {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Field {
+
+    protected String name;
+    protected String type;
+}
+
+class Class {
+    protected String name;
+}
+
+
+class CodeBuilder {
+
+    public CodeBuilder(String className) {
+
+    }
+
+    public CodeBuilder addField(String name, String type) {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+}
