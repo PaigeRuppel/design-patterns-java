019a0b8023ff8bf8349624bfc1dd9489ddcc702d
Paige Ruppel <paige.ruppel@gmail.com>
2018-03-01 08:49:31 -0500

    added caching to adapter

--- src/main/java/com/paigeruppel/udemy/designpatterns/structural/adapter/VectorRasterAdapterDemo.java
+++ src/main/java/com/paigeruppel/udemy/designpatterns/structural/adapter/VectorRasterAdapterDemo.java
@@ -1,8 +1,7 @@
 package com.paigeruppel.udemy.designpatterns.structural.adapter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
 
 public class VectorRasterAdapterDemo {
     private final static List<VectorObject> vectorObjects
@@ -17,13 +16,14 @@ public class VectorRasterAdapterDemo {
 
     private static void draw() {
         for (VectorObject vo : vectorObjects) {
-            for (Line l : vo) {
-                LineToPointAdapter adapter = new LineToPointAdapter(l);
+            for (Line line : vo) {
+                LineToPointAdapter adapter = new LineToPointAdapter(line);
                 adapter.forEach(VectorRasterAdapterDemo::drawPoint);
             }
         }
     }
     public static void main(String[] args) {
+        draw();
         draw();
     }
 }
@@ -36,20 +36,22 @@ class Point {
         this.y = y;
     }
 
-    public int getX() {
-        return x;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Point point = (Point) o;
+
+        if (x != point.x) return false;
+        return y == point.y;
     }
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
     }
 
     @Override
@@ -81,6 +83,24 @@ class Line {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Line line = (Line) o;
+
+        if (start != null ? !start.equals(line.start) : line.start != null) return false;
+        return end != null ? end.equals(line.end) : line.end == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = start != null ? start.hashCode() : 0;
+        result = 31 * result + (end != null ? end.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "Line{" +
                 "start=" + start +
@@ -89,14 +109,20 @@ class Line {
     }
 }
 
-class LineToPointAdapter extends ArrayList<Point> {
+class LineToPointAdapter implements Iterable<Point> {
 
     private static int count = 0;
+    private static Map<Integer, List<Point>> cache = new HashMap<>();
+    private int hash;
 
     public LineToPointAdapter(Line line) {
+        hash = line.hashCode();
+        if (cache.get(hash) != null) return; // this means the hash already exists
         System.out.println(
-                String.format("%d: Generating points for line [%d,%d]-[%d,%d] (no caching)",
+                String.format("%d: Generating points for line [%d,%d]-[%d,%d] (with caching)",
         ++count, line.start.x, line.start.y, line.end.x, line.end.y));
+
+        ArrayList<Point> points = new ArrayList<>();
 
         int left = Math.min(line.start.x, line.end.x);
         int right = Math.max(line.start.x, line.end.x);
@@ -107,12 +133,30 @@ class LineToPointAdapter extends ArrayList<Point> {
 
         if (dx == 0) {
             for (int y = top; y <= bottom; ++y) {
-                add(new Point(left, y));
+                points.add(new Point(left, y));
             }
         } else if (dy == 0) {
             for (int x = left; x <= right; ++x) {
-                add(new Point(x, top));
+                points.add(new Point(x, top));
             }
         }
+
+        // hash tells you what hash corresponds to the line you've made
+        cache.put(hash, points);
+    }
+
+    @Override
+    public Iterator<Point> iterator() {
+        return cache.get(hash).iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Point> action) {
+        cache.get(hash).forEach(action);
+    }
+
+    @Override
+    public Spliterator<Point> spliterator() {
+        return cache.get(hash).spliterator();
     }
 }
