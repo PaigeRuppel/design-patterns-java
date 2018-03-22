76083899538f72a3a4b0f37faba29be30070ac10
Paige Ruppel <paige.ruppel@gmail.com>
2018-03-22 14:32:22 -0400

    implemented a simple command on bank account

--- src/main/java/com/paigeruppel/udemy/designpatterns/behavioral/command/BankAccountCommandDemo.java
+++ src/main/java/com/paigeruppel/udemy/designpatterns/behavioral/command/BankAccountCommandDemo.java
@@ -1,6 +1,20 @@
 package com.paigeruppel.udemy.designpatterns.behavioral.command;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BankAccountCommandDemo {
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        System.out.println(bankAccount);
+
+        List<BankAccountCommand> commands = Arrays.asList(new BankAccountCommand(bankAccount, BankAccountCommand.Action.DEPOSIT, 100), new BankAccountCommand(bankAccount, BankAccountCommand.Action.WITHDRAW, 1000));
+
+        for (BankAccountCommand c : commands) {
+            c.execute();
+            System.out.println(bankAccount);
+        }
+    }
 }
 
 class BankAccount {
@@ -26,5 +40,39 @@ class BankAccount {
         return "BankAccount{" +
                 "balance=" + balance +
                 '}';
+    }
+}
+
+interface Command {
+    void execute();
+}
+
+class BankAccountCommand implements Command {
+    private BankAccount account;
+
+
+
+    public enum Action {
+        DEPOSIT, WITHDRAW;
+    }
+    private Action action;
+
+    private int amount;
+    public BankAccountCommand(BankAccount account, Action action, int amount) {
+        this.account = account;
+        this.action = action;
+        this.amount = amount;
+    }
+
+    @Override
+    public void execute() {
+        switch (action){
+            case DEPOSIT:
+                account.deposit(amount);
+                break;
+            case WITHDRAW:
+                account.withdraw(amount);
+                break;
+        }
     }
 }
